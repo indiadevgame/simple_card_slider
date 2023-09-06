7ff42a8e2900868a76e63e2379125add4f9b5c30
Sarthak Sachdeva <140259749+indiadevgame@users.noreply.github.com>
2023-09-06 04:58:41 -0700

    Update script.js

--- card_slider/script.js
+++ card_slider/script.js
@@ -11,8 +11,9 @@ function prev(){
     slider.prepend(slider_card[slider_card.length -1])
 }
 
+// this is for run slider automatic
 
-// let auto = setInterval(()=>{
-//     slider.append(slider_card[0]);
+let auto = setInterval(()=>{
+    slider.append(slider_card[0]);
     
-// } , 1000)
+} , 5000)
